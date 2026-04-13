cf66f057a8f52f20821d0f5bdbeadf4c63777c67
nandhiraja <nandhiraja16@gmail.com>
2026-04-13 20:32:31 +0530

    refactor[scrape module]: Remove debug print statements and make as module

--- task-01/scraper/extractor.py
+++ task-01/scraper/extractor.py
@@ -1,28 +1,30 @@
 from playwright.sync_api import sync_playwright
 
-URL = 'https://books.toscrape.com'
 
 def scrape_site(URL):
     print('scrapping starts.....')
-    
+    products = []
     with sync_playwright() as p :
         browser = p.chromium.launch(headless=False)
         page =  browser.new_page()
-        print('Going to site ...')
+        print('Going to site : ',URL)
         page.goto(URL)
 
-        print('wait for product load...')
         page.wait_for_selector('.product_pod')
         items = page.locator('.product_pod')
-        print('printing items .......')
         for i in range(items.count()):
             item = items.nth(i)
             name = item.locator("h3 a").get_attribute("title")
             price = item.locator(".price_color").inner_text()
-            print('\n------------------------Item ',i,'---------------------------------')
-            print(name)
-            print(price)
+            instock = item.locator('.availability').inner_text()
+            item_detail ={
+                'name': name.strip(),
+                'price':price.strip(),
+                'sku' : instock.strip()
+            }
+            products.append(item_detail)
         
         browser.close()
+        return products
     
-scrape_site(URL)
+# print(scrape_site(URL))
