15eeb8e8566e9e2944aea89b23f766c6c8aa2909
nandhiraja <nandhiraja16@gmail.com>
2026-04-13 16:37:51 +0530

    feat[DataBase]: Add functionality to update the data to database table

---
+++ task-01/db/db.py
@@ -0,0 +1,31 @@
+import sqlite3
+from datetime import datetime
+from db.db_model import CREATE_TABLE
+
+DB_PATH  = 'Product.db'
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
+
+
+
+def update_product(product):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(CREATE_TABLE)
+
+    cur.execute("""
+    INSERT INTO products (sku, name, price, last_updated)
+    VALUES (?, ?, ?, ?)   ON CONFLICT(sku) DO UPDATE SET 
+         price=excluded.price,
+        last_updated=excluded.last_updated
+    """, (
+        product["sku"],
+        product["name"],
+        float(product["price"].replace("$", "")),
+        datetime.now()
+    ))
+
+    conn.commit()
+    conn.close()
