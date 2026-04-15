6e6ba8c5c6c68393d1b66fc90cb43ad827104145
nandhiraja <nandhiraja16@gmail.com>
2026-04-15 23:20:42 +0530

    feat[main.py]: Add funtionality to join a room and send message in room members

--- task-02/backend/main.py
+++ task-02/backend/main.py
@@ -4,6 +4,12 @@ app = FastAPI()
 
 class ConnectionManager:
     def __init__(self):
+        self.rooms:dict= {'general':[],
+                          'office':[],
+                          'coder':[]
+                          }
+        
+        self.user_map: dict ={}
         self.active_connections: dict = {} 
 
     async def connect(self, user_id: str, websocket: WebSocket):
@@ -12,6 +18,18 @@ class ConnectionManager:
 
     def disconnect(self, user_id: str):
         del self.active_connections[user_id]
+    
+    async def join_room(self,room,websocket: WebSocket):
+        if room not in self.rooms:
+            self.rooms[room]=[]
+        self.rooms[room].append(websocket)
+        print(self.rooms)
+
+    async def broadcast(self, room: str, message: dict):
+        if room in self.rooms:
+            for connection in self.rooms[room]:
+                await connection.send_json(message)
+
 
     async def send_personal_message(self, message: str, user_id: str):
         if user_id in self.active_connections:
@@ -25,15 +43,21 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str):
     await manager.connect(user_id, websocket)
     try:
         while True:
-            data = json.loads(await websocket.receive_text())   
-            print(data) 
-            message = {'user':data['user'],
-                       'text': data['message'],
-                       'room': data['room'] ,
-                       'timestamp': data['timestamp']
-                       } 
-            print(message) 
-            await manager.send_personal_message(message, data['receiver'] )
+            data = json.loads(await websocket.receive_text())
+            if(data['type']=='room-join'):
+                await manager.join_room(data['room'],websocket)
+
+            elif(data['type']=='chat-message'):
+                print(data) 
+                message = {'user':data['user'],
+                           'text': data['message'],
+                           'room': data['room'] ,
+                           'timestamp': data['timestamp']
+                           } 
+                print(message) 
+                await manager.broadcast(data['room'],message )
+            else:
+                print("Sorry no process......")
 
 
     except WebSocketDisconnect:
