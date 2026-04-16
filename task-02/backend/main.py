40687432b5bdf9d988a29076c28ffbb7f0d96dcb
nandhiraja <nandhiraja16@gmail.com>
2026-04-16 22:50:06 +0530

    feat: Add functionality to store and share user joined rooms

--- task-02/backend/main.py
+++ task-02/backend/main.py
@@ -27,11 +27,28 @@ class ConnectionManager:
     def disconnect(self, user_id: str):
         del self.active_connections[user_id]
     
-    async def join_room(self,room,websocket: WebSocket):
+    async def join_room(self,user_id,room,websocket: WebSocket):
         if room not in self.rooms:
             self.rooms[room]=[]
+        if websocket in self.rooms[room]:
+            print('Already user present..')
+            return
+
         self.rooms[room].append(websocket)
-        print(self.rooms)
+        self.user_map[user_id].append(room)
+        await self.send_activate_room(user_id,websocket)
+
+    async def send_activate_room(self,user_id,websocket):
+        if user_id not in self.user_map:
+            self.user_map[user_id]=[]
+        message ={
+            'type':"activate-room",
+            "user":user_id,
+            'activatedRoom':self.user_map[user_id]
+        }
+        print(message)
+        await websocket.send_json(message)
+        
 
     async def broadcast(self, room: str, message: dict):
         if room in self.rooms:
@@ -61,6 +78,15 @@ class ConnectionManager:
             'history': self.rooms_history[room]
         }
         await websocket.send_json(message)
+    async def send_typing_info(self,user_id,room):
+        message={
+            'type':'typing',
+             'user':user_id,
+             'room':room
+        }
+        if room in self.rooms:
+            for connection in self.rooms[room]:
+                await connection.send_json(message)
 
     async def update_history(self,room,message):
         self.rooms_history[room].append(message)
@@ -70,11 +96,12 @@ manager = ConnectionManager()
 @app.websocket("/ws/{user_id}")
 async def websocket_endpoint(websocket: WebSocket, user_id: str):
     await manager.connect(user_id, websocket)
+    await manager.send_activate_room(user_id,websocket)
     try:
         while True:
             data = json.loads(await websocket.receive_text())
             if(data['type']=='room-join'):
-                await manager.join_room(data['room'].lower(),websocket)
+                await manager.join_room(data['user'],data['room'].lower(),websocket)
 
             elif(data['type']=='chat-message'):
                 print(data) 
@@ -82,7 +109,8 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str):
                             'user':data['user'],
                            'text': data['message'],
                            'room': data['room'] ,
-                           'timestamp': data['timestamp']
+                           'timestamp': data['timestamp'],
+                            'online':len(manager.active_connections)
                            } 
                 print(message) 
                 await manager.broadcast(data['room'].lower(),message )
@@ -94,7 +122,8 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str):
 
             elif(data['type']=='chat-history'):
                 await manager.send_history(data['room'].lower(),websocket)              
-                 
+            elif(data['type']=='typing'):
+                await manager.send_typing_info(data['user'],data['room'])
             else:
                 print("Sorry no process......")
 
