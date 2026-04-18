5975b27a04c8e7152ac0f672ad60925f45db7e85
nandhiraja <nandhiraja16@gmail.com>
2026-04-18 22:33:41 +0530

    feat: Update worker functionality  to complete task in queue

--- task-04/worker.py
+++ task-04/worker.py
@@ -1,5 +1,7 @@
 from task import FUNCTIONS
-
+from redis_db import redis_connection,DEAD_TASK_NAME,QUEUE_NAME
+import time
+import json
 def process_task(task):
     func_name = task["func"]
     args = task["args"]
@@ -11,3 +13,36 @@ def process_task(task):
         raise Exception("Function not found...")
 
     return func(**args)
+
+def worker(worker_id):
+
+
+    while True:
+        task_data = redis_connection.rpop(QUEUE_NAME)
+
+        if not task_data:
+            time.sleep(1)
+            continue
+
+        task = json.loads(task_data)
+        task_id = task["id"]
+
+
+        start_time = time.time()
+
+        try:
+            result = process_task(task)
+            duration = round(time.time() - start_time, 2)
+
+
+        except Exception as e:
+            task["retries"] += 1
+
+            if task["retries"] <= task["max_retries"]:
+                delay = 2 ** task["retries"]
+
+                time.sleep(delay)
+                redis_connection.lpush(QUEUE_NAME, json.dumps(task))
+
+            else:
+                redis_connection.lpush(DEAD_TASK_NAME, json.dumps(task))
