37983bdbc792cda7c643e1efcf4f11906749c140
nandhiraja <nandhiraja16@gmail.com>
2026-04-18 23:01:07 +0530

    Chore: Add dummy task to test the system

--- task-04/main.py
+++ task-04/main.py
@@ -14,4 +14,18 @@ def enqueue(func_name, **kwargs):
     id+=1
     
     redis_connection.lpush(QUEUE_NAME, json.dumps(task))
+    print(f"Task Queued : {task['id']} ({func_name})")
 
+if __name__ == "__main__":
+    from multiprocessing import Process
+    import time
+    from worker import worker
+
+    for i in range(3):
+        Process(target=worker, args=(i+1,)).start()
+
+    time.sleep(2)
+
+    enqueue("add", a=5, b=10)
+    enqueue("generate_thumbnail", image_id=4521, size=(256, 256))
+    enqueue("send_email", to="bob@co.com", template="welcome")
