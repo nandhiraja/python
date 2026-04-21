892a9955b0e0d25daebdcbe86facf54042a4e873
nandhiraja <nandhiraja16@gmail.com>
2026-04-21 22:20:02 +0530

    feat: data preprocess addtional feature column transform function  has added

--- task-05/preprocess_data.py
+++ task-05/preprocess_data.py
@@ -1,5 +1,6 @@
 from sklearn.impute import SimpleImputer
-
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
 
 def clean_data(df):
     num_cols = df.select_dtypes(include=['int64', 'float64']).columns
@@ -12,3 +13,16 @@ def clean_data(df):
     df[cat_cols] = cat_imputer.fit_transform(df[cat_cols])
 
     return df
+
+def column_traform (num_cols,cat_cols):
+    
+
+    numeric_features = num_cols
+    categorical_features = cat_cols
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', StandardScaler(), numeric_features),
+            ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
+        ]
+    )
