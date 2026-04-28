2654f359c8bfdc4e4d6c276ee1265b8038ca592f
nandhiraja <nandhiraja16@gmail.com>
2026-04-28 22:27:31 +0530

    refactor: organize the preprocess functionaliy in src/data/

---
+++ task-05/src/data/preprocess_data.py
@@ -0,0 +1,37 @@
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+def clean_data(df):
+
+    num_cols = df.select_dtypes(include=['int64', 'float64']).columns
+    cat_cols = df.select_dtypes(include=['object']).columns
+
+    num_imputer = SimpleImputer(strategy='median')
+    cat_imputer = SimpleImputer(strategy='most_frequent')
+
+    df[num_cols] = num_imputer.fit_transform(df[num_cols])
+    df[cat_cols] = cat_imputer.fit_transform(df[cat_cols])
+
+    return df
+
+
+def column_transform(df):
+
+    numeric_features = df.select_dtypes(
+        include=['int64', 'float64']
+    ).drop("churn", axis=1).columns
+
+    categorical_features = df.select_dtypes(
+        include=['object']
+    ).columns
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", StandardScaler(), numeric_features),
+            ("cat", OneHotEncoder(handle_unknown='ignore'),
+             categorical_features)
+        ]
+    )
+
+    return preprocessor
